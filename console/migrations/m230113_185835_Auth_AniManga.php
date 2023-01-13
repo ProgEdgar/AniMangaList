fc6884cd64aa3c77c4fe31d20066c3abe33d56c3
Edgar <prog.edgar@hotmail.com>
2023-01-13 20:02:33 +0000

    Migrations (With some kind of error)

---
+++ console/migrations/m230113_185835_Auth_AniManga.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230113_185835_Auth_AniManga
+ */
+class m230113_185835_Auth_AniManga extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m230113_185835_Auth_AniManga cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230113_185835_Auth_AniManga cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
---
+++ console/migrations/m230113_185853_ChEp_AniManga.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230113_185853_ChEp_AniManga
+ */
+class m230113_185853_ChEp_AniManga extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m230113_185853_ChEp_AniManga cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230113_185853_ChEp_AniManga cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
